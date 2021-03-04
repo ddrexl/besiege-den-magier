91022ed4f050e85cee150ef732d09b69c39a22b5
Dominik Drexl <domdrexl@gmail.com>
2021-03-04 21:43:26 +0100

    No low processor usage mode for android

--- Game/TitleScreen.gd
+++ Game/TitleScreen.gd
@@ -5,6 +5,7 @@ onready var start_button = $Menu/CenterRow/VBoxContainer/Buttons/StartButton
 onready var change_level = $ChangeLevel
 
 func _ready():
+	OS.low_processor_usage_mode = OS.get_name() != "Android"
 	start_button.grab_focus()
 	for button in menu_buttons.get_children():
 		button.connect("pressed", self, "_on_Button_selected", [button.scene_to_load])
